7f40c41430e8fcbef44d375eb9236c6429f1800d
Olufunsho Olagundoye <Funsholag@gmail.com>
2018-02-04 20:07:52 -0500

    made changes to staff page, created mission page, added image and text to mission page

---
+++ morning-star/app/controllers/sermon_controller.rb
@@ -0,0 +1,5 @@
+class SermonController < ApplicationController
+  def index
+
+  end
+end
--- morning-star/config/routes.rb
+++ morning-star/config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   get '/' => "home#index"
   root :to => "home#index"
 
+  get '/sermon' => "sermon#index"
+
   get '/about' => "about#index"
   get '/about/history' => "about#history"
+  get '/about/mission' => "about#mission"
   get '/about/staff' => "about#staff"
   get '/event' => "event#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
